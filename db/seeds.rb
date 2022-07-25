b3bedd6b6f556a3f23a2ff3a212159a74e7f06b2
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-25 16:22:55 -0500

    populating db in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+isaac = User.create(name: 'Isaac')
+juan = User.create(name: 'Juan')
+pepe = User.create(name: 'Pepe')
+augusto = User.create(name: 'Augusto')
+jorge = User.create(name: 'Jorge')
+
+food1 = Food.create(name: 'food 1', measurement_unit:'kg', price: 1.5, quantity:1, user: isaac)
+food2 = Food.create(name: 'food 2', measurement_unit:'kg', price: 2.5, quantity:2, user: juan)
+food3 = Food.create(name: 'food 3', measurement_unit:'kg', price: 3.5, quantity:3, user: pepe)
+food4 = Food.create(name: 'food 4', measurement_unit:'kg', price: 4.5, quantity:4, user: augusto)
+food5 = Food.create(name: 'food 5', measurement_unit:'kg', price: 5.5, quantity:5, user: jorge)
