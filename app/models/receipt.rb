25f80421ba67f9e92da616f1d8d4b21c0835af15
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-26 11:10:16 -0500

    Receipt model added

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,2 +1,7 @@
 class Receipt < ApplicationRecord
+    belongs_to :user
+    has_many :recipe_foods, dependent: :destroy
+
+    validates :name, presence: true, length: { maximum: 25 }
+    validates :description, presence: true, length: { maximum: 500 }
 end
