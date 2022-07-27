be39117b96f335238da4577f6547ff6e5001c5ed
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-27 12:23:28 -0500

    devise & add food working

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,13 +1,25 @@
 class FoodsController < ApplicationController
   def index
-    @foods = Food.all
+    @food = current_user.foods.all
   end
 
   def new
     @food = Food.new
   end
 
-  def create; end
+  def create
+    @food = Food.new(food_params)
+    @food.user = current_user
+    if @food.save
+      redirect_to foods_path
+    else
+      render :new
+    end
+  end
 
   def destroy; end
+
+  def food_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root "foods#index"
+  resources :users do 
+    resources :foods, only: [:index, :new, :create, :destroy]
+    resources :receipts, only:[:index, :show] 
+    resources :publics, only:[:index]
+    resources :shoppings, only:[:index]
+  end
+
   resources :foods, only: [:index, :new, :create, :destroy]
-  resources :receipts, only:[:index, :show] 
-  resources :publics, only:[:index]
-  resources :shoppings, only:[:index]
 end
