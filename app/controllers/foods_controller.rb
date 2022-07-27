8b644d8bb76d8968d8bc184c245b77963c7343f5
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-27 12:25:09 -0500

    delete button working

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -17,7 +17,11 @@ class FoodsController < ApplicationController
     end
   end
 
-  def destroy; end
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
+    redirect_to foods_path
+  end
 
   def food_params
     params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
