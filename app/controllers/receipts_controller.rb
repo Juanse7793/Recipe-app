d1394e345681e4e28f3911126785f49ba11e4085
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-27 16:56:11 -0500

    trying to add RecipeFoods

--- app/controllers/receipts_controller.rb
+++ app/controllers/receipts_controller.rb
@@ -7,10 +7,30 @@ class ReceiptsController < ApplicationController
     @receipt = Receipt.find(params[:id])
   end
 
+  def new
+    @receipt = RecipeFood.new
+    @foods = Food.all
+  end
+
+  def create
+    @receiptfood = RecipeFood.new(recipe_food_params)
+    @receiptfood.receipt = Receipt.find(params[:receipt_id])
+    @receiptfood.food = Food.find(recipe_food_params[:food_id])
+    if @receiptfood.save
+      redirect_to receipts_path
+    else
+      render :new
+    end
+  end
+
   def remove_food
     @receipt = Receipt.find(params[:receipt_id])
     @food = RecipeFood.find(params[:id])
     @receipt.recipe_foods.delete(@food)
     redirect_to user_receipt_path
+  end
+
+  def recipe_food_params
+    params.require(:recipe_food).permit(:quantity, :food_id)
   end
 end
