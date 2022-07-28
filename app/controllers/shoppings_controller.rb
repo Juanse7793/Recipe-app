538d87f6f3b9f732df1ea5fb83e2184127f19e0c
isaacpitwa <isaacpitwa256@gmail.com>
2022-07-29 01:17:36 +0300

    fix rubocop erros

--- app/controllers/shoppings_controller.rb
+++ app/controllers/shoppings_controller.rb
@@ -6,11 +6,13 @@ class ShoppingsController < ApplicationController
     @recipes.each do |recipe|
       recipe.recipe_foods.includes([:food]).each do |recipe_food|
         if recipe_food.quantity > recipe_food.food.quantity
-          shopping_item  = ShoppingItem.new(recipe_food.food,recipe_food.quantity - recipe_food.food.quantity)
+          shopping_item = ShoppingItem.new(recipe_food.food, recipe_food.quantity - recipe_food.food.quantity)
           @shoppings << shopping_item
         end
       end
     end
-    @total_price = @shoppings.inject(0) { |sum, shopping_item| sum +  shopping_item.food.price * shopping_item.requiredQuantity}
+    @total_price = @shoppings.inject(0) do |sum, shopping_item|
+      sum + (shopping_item.food.price * shopping_item.required_quantity)
+    end
   end
 end
--- app/models/shopping_item.rb
+++ app/models/shopping_item.rb
@@ -1,7 +1,8 @@
-class ShoppingItem 
-    attr_accessor :requiredQuantity, :food
-    def initialize(food,requiredQuantity)
-      @food=food;
-      @requiredQuantity = requiredQuantity
-    end
+class ShoppingItem
+  attr_accessor :required_quantity, :food
+
+  def initialize(food, required_quantity)
+    @food = food
+    @required_quantity = required_quantity
+  end
 end
