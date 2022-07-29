4c89c00db81c8a7326328c887f70e3b561d032ef
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-07-29 11:44:53 -0500

    mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   # Config default mailer url
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { :host => "dev.yourhost.com" }
 
   # Config default mailer delivery method
   config.action_mailer.delivery_method = :letter_opener
